32554a1293addf4f88e015009958bd5f0164b70b
sbhattacharyya2014 <s.bhattacharyya2014@gmail.com>
2020-06-04 20:42:29 +0530

    modification

--- src/orderDetails/OrderDetails.java
+++ src/orderDetails/OrderDetails.java
@@ -29,11 +29,17 @@ public class OrderDetails {
 		Map<String, List<String>> itemInOrders = new HashMap<String, List<String>>();
 		List<String> items = new ArrayList<String>();
 		//Finding the unique items in all orders
-		for (Order od : orders) {
-			for (String item : od.getItemsInOrder()) {
-				if (!items.contains(item))
-					items.add(item);
+		try {
+			for (Order od : orders) {
+				for (String item : od.getItemsInOrder()) {
+					if (!items.contains(item))
+						items.add(item);
+				}
 			}
+		} catch (NullPointerException e) {
+			// TODO Auto-generated catch block
+			System.out.println("An order item can not be null! Please check the input");
+			System.exit(1);
 		}
 		//Adding the items and orders as key value pair
 		for (String item : items) {
@@ -60,6 +66,10 @@ public class OrderDetails {
 
 
 	}
+	
+	public void addOrder(Order order) {
+		this.orders.add(order);
+		}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -67,7 +77,9 @@ public class OrderDetails {
 		Order od2 = new Order("order 2", Arrays.asList("item 3"));
 		Order od3 = new Order("order 3", Arrays.asList("item 2"));
 		Order od4 = new Order("order 4", Arrays.asList("item 1", "item 2"));
-		OrderDetails oDetails = new OrderDetails(Arrays.asList(od1, od2, od3,od4));
+		OrderDetails oDetails = new OrderDetails(new ArrayList<Order>(Arrays.asList(od1, od2, od3,od4)));
+		Order od5 = new Order("order 5", Arrays.asList("item 1"));
+		oDetails.addOrder(od5);
 		oDetails.highestFrequency();
 
 	}
